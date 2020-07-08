aec298f050edb34b6d3e9906db1a99d8d8b1da72
Ivan Rossi <ivanross.me@gmail.com>
2020-07-08 09:56:55 +0200

    ✅add more parse tests

--- tests/parse.test.js
+++ tests/parse.test.js
@@ -3,11 +3,19 @@ import { parseAttributeName, parseEventName } from '../src/parse'
 describe(parseAttributeName, () => {
   it('should convert camel case to kebab case', () => {
     expect(parseAttributeName('fontSize')).toEqual('font-size')
+    expect(parseAttributeName('strokeWidth')).toEqual('stroke-width')
+    expect(parseAttributeName('dominantBaseline')).toEqual('dominant-baseline')
     expect(parseAttributeName('HTMLInputElement')).toEqual('html-input-element')
   })
 
   it('should keep numbers', () => {
     expect(parseAttributeName('x1')).toEqual('x1')
+  })
+
+  it('should keep attribute name if already parsed', () => {
+    expect(parseAttributeName('x')).toEqual('x')
+    expect(parseAttributeName('stroke')).toEqual('stroke')
+    expect(parseAttributeName('stroke-width')).toEqual('stroke-width')
   })
 })
 
@@ -17,7 +25,7 @@ describe(parseEventName, () => {
     expect(parseEventName('onMouseDown')).toEqual('mousedown')
   })
 
-  it('should convert camel case to kebab case', () => {
+  it('should keep event name if already parsed', () => {
     expect(parseEventName('click')).toEqual('click')
     expect(parseEventName('mousedown')).toEqual('mousedown')
   })
